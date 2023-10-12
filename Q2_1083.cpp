871dcae5428123c3e16ee22563f3f7af9bfd13f7
mr0crappy <kpratyush82@gmail.com>
2023-10-13 03:49:37 +0530

    added q2

---
+++ Q2_1083.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    long int x{}, nums{}, usersum{0}, sum{0};
+    std::cin >> x;
+    for (int i = 1; i < x; i++) {
+       std::cin >> nums;
+       usersum += nums;
+    }
+    sum = (x*(x+1))/2;      //finds the sum of all numbers in range
+    //subtracting sum of all the numbers input from the total sum                        
+    std::cout << sum-usersum;          
+}
