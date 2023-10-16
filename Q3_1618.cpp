8d84ba1d53c831f4e8f7852902107718f16c285c
mr0crappy <kpratyush82@gmail.com>
2023-10-16 22:46:52 +0530

    added q3

---
+++ Q3_1618.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{ 
+    long long int t{0}, u{0};
+    int x, fv;
+    std::cin >> x;
+    fv=x;
+    while (x!=0) {
+        x=x/2;
+        t+=x;
+    }
+    while (fv!=0) {
+        fv=fv/5;
+        u+=fv;
+    }
+    if (t<x) {
+        std::cout << t;
+    }
+    else {
+        std::cout << u;
+    }
+}
