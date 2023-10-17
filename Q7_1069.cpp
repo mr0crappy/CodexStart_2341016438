d4297398e08ea8d1cb6f70718441200a4d9c4d70
mr0crappy <kpratyush82@gmail.com>
2023-10-17 21:53:42 +0530

    Q7

---
+++ Q7_1069.cpp
@@ -0,0 +1,19 @@
+#include <iostream>                                                                                                                                                                                                                        
+
+int main() {
+    std::string s;
+    char current;
+    int count = 0, answer = 0;
+    std::cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != current) {
+            current = s[i];
+            count = 0;
+        }
+        if (s[i] == current) {
+            count++;
+        }
+        answer = std::max(answer, count);
+    }
+    std::cout << answer;
+}
