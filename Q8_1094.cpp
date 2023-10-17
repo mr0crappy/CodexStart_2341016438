10e8a6706259c34591e539ceae600b437630afa5
mr0crappy <kpratyush82@gmail.com>
2023-10-17 22:13:37 +0530

    Q8

---
+++ Q8_1094.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    long long n, x, answer = 0;
+    std::cin >> n;
+    long long values[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> values[i];
+    }
+    for (int i = 1; i < n; i++) {
+        if (values[i] < values[i - 1]) {
+            answer += (values[i - 1] - values[i]);
+            values[i] = values[i - 1];
+        }
+    }
+    std::cout << answer;
+}
